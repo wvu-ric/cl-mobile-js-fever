824d1da6c6c800d82d71cdfbb30603086c4df9dc
stadelmanma <stadelmanma@gmail.com>
2016-01-19 18:21:33 -0500

    form logic added

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 "use strict";
 //
-// getting user input
-var tempString = prompt("What is your temperature?");
-//
-// converting number
-var tempNum = Number(tempString);
-//
-// checking value of number
-if (tempNum < 98.6) {
-    alert('Your temperture is too low!');
-}
-else if (tempNum == 98.6) {
-    alert('Your temperature is just right.');
-}
-else if (tempNum > 98.6) {
-    alert('Your temperature is too high!')
-}
-else {
-    alert('Invalid number entered: '+tempString+' -> '+tempNum)
+function check_temp() {
+    //
+    // getting user input
+    var tempString = document.getElementById('user-temp').value;
+    //
+    // converting number
+    var tempNum = Number(tempString);
+    //
+    // checking value of number
+    if (tempNum < 98.6) {
+        message = 'Your temperture is too low!';
+    }
+    else if (tempNum == 98.6) {
+        message = 'Your temperature is just right.';
+    }
+    else if (tempNum > 98.6) {
+        message = 'Your temperature is too high!';
+    }
+    else {
+        message = 'Invalid number entered: '+tempString+' -> '+tempNum;
+    }
+    //
+    // outputting value
+    document.getElementById('message').innerHTML = message;
 }
