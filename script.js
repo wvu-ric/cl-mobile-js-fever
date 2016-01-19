82438e52ec1164b02a025582f22c61d5ce9b7052
stadelmanma <stadelmanma@gmail.com>
2016-01-19 18:02:48 -0500

    initial changes to implement logic

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+"use strict";
+//
+// getting user input
 var tempString = prompt("What is your temperature?");
-
-var tempNum = Number(tempString)
-
-
-// Put your if logic here
+//
+// converting number
+var tempNum = Number(tempString);
+//
+// checking value of number
+if (tempNum < 98.6) {
+    alert('Your temperture is too low!');
+}
+else if (tempNum == 98.6) {
+    alert('Your temperature is just right.');
+}
+else if (tempNum > 98.6) {
+    alert('Your temperature is too high!')
+}
+else {
+    alert('Invalid number entered: '+tempString+' -> '+tempNum)
+}
