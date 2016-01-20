9326009f96d001fb1a52b3f0de6401400f60a507
jmathen2 <jmathen2@mix.wvu.edu>
2016-01-19 22:07:09 -0500

    "Fever" Assignment

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+//Josh Matheny "Fever" Assignment
 var tempString = prompt("What is your temperature?");
 
 var tempNum = Number(tempString)
 
+//Chose a value for the normal body temp of the user
+var bodytemp = 98.6;
 
-// Put your if logic here
+//Checking to see if User's temp is okay...
+if (tempNum === bodytemp) {
+  alert("The users temperature is just right...");
+} else {
+  //Checking to see if users temp is too high...
+  if (tempNum > bodytemp) {
+    alert("The users temperature is high")
+  } else {
+    //Checking to see if users temp is too low...
+    if (tempNum < bodytemp) {
+      alert("The users temperature is low")
+    }
+    }
+  }
