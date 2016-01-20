5b1292e9cee53596a98808f872c55021f45e7a71
mckinneyra <ramckinney@mix.wvu.edu>
2016-01-19 23:04:11 -0500

    Assignment complete

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ var tempNum = Number(tempString)
 
 
 // Put your if logic here
+if (tempNum <= 98.6) {
+  alert("You do not have a fever");
+}
+
+if (tempNum > 98.6) {
+  alert("You do have a fever");
+}
