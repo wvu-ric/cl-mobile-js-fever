bc62015acda97057849d40e776916c67c6a2e682
jcordonier <jcordoni@itsc1mhwmfedty3.wvu-ad.wvu.edu>
2016-01-21 17:14:02 -0500

    fever program

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var tempString = prompt("What is your temperature?");
 
-var tempNum = Number(tempString)
+var tempNum = Number(tempString);
 
 
 // Put your if logic here
+
+if(tempNum < 98.6)''
+    alert("Your temperature is too low!");
