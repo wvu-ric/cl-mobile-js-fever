b01949be92f380e2ba15f8fbc0ea9f7d4dcad37d
jcordonier <jcordoni@itsc1mhwmfedty3.wvu-ad.wvu.edu>
2016-01-26 16:59:58 -0500

    Fever

--- script.js
+++ script.js
@@ -5,5 +5,14 @@ var tempNum = Number(tempString);
 
 // Put your if logic here
 
-if(tempNum < 98.6)''
-    alert("Your temperature is too low!");
+if(tempNum < 98){
+  alert("Your temperature is too low for a fever!");
+}
+
+else if(tempNum == 98){
+    alert("Your temperature is OK.")
+}
+
+else if(tempNum > 98){
+alert("Your temperature is high!  You might have a fever.");
+}
