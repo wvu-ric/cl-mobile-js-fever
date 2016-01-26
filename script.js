fd22735ac754f2b3498e3c76e52415b0e0a05790
NicCronin <nicronin@mix.wvu.edu>
2016-01-26 17:04:16 -0500

    Fever Temp (High, Low or Just Right)

--- script.js
+++ script.js
@@ -3,4 +3,14 @@ var tempString = prompt("What is your temperature?");
 var tempNum = Number(tempString)
 
 
-// Put your if logic here
+if (tempNum == 98) {
+  alert("Just right!")
+}
+
+if (tempNum < 98) {
+  alert("Too low!")
+}
+
+if(tempNum > 98) {
+  alert("Too high!")
+}
