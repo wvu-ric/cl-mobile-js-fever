48b6209cac25dbc1245e3bde35b2f639c59515d0
Nancy Andrews <nancy@nancyandrews.com>
2016-02-18 16:05:02 -0500

    completed assignment

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var tempString = prompt("What is your temperature?");
 // this is a comment
-var tempNum = Number(tempString)
+var tempNum = Number(tempString);
 
-
-// Put your if logic here
+if (tempNum <= 98.6) {
+alert("Congratulations, you do not have a fever")
+} else {
+  alert("Oops, you have a fever, go ahead and skip class");
+}
