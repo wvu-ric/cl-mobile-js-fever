6891e51d749dc3c1afb47e9f79215355b9da1dcc
Karri <khitt@k12.wv.us>
2016-06-29 14:34:54 -0400

    if statement added

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 var tempString = prompt("What is your temperature?");
 
-var tempNum = Number(tempString)
+var tempNum = Number(tempString);
 
+if (tempNum > 98.6) {
+	alert ("Your temperature is high.");
+} else if (tempNum == 98.6) {
+		alert ("Your temperature is just right!");
+} else {
+	alert ("Your temperature is low.");
+}
 
-// Put your if logic here
